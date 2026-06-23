18181490bbc8736b30ec68c86f1b722bb77e260e
Haneul Lee <52236416+skythehuman@users.noreply.github.com>
2026-06-23 22:19:20 +0900

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Seoul-Noise-Health-Risk.v2/',
+  base: 'skythehuman/Seoul-Noise-Health-Risk.v2/',
 })
